8c0b03a00dce027636286bb2400f874394cae5be
altkachuk <andreyltkachuk@gmail.com>
2026-01-13 22:09:41 +0100

    updated deps

---
+++ scripts/setup_system.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+apt-get update
+apt-get install \
+    git \
+    cmake \
+    ninja-build \
+    build-essential \
+    libboost-program-options-dev \
+    libboost-graph-dev \
+    libboost-system-dev \
+    libeigen3-dev \
+    libfreeimage-dev \
+    libmetis-dev \
+    libgoogle-glog-dev \
+    libgtest-dev \
+    libgmock-dev \
+    libsqlite3-dev \
+    libglew-dev \
+    qt6-base-dev \
+    libqt6opengl6-dev \
+    libqt6openglwidgets6 \
+    libcgal-dev \
+    libceres-dev \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    libmkl-full-dev
