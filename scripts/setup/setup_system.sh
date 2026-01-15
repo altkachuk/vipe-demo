23f7e8996f9f3688ec21e162ab7b3274ebe8afa6
altkachuk <andreyltkachuk@gmail.com>
2026-01-15 17:11:38 +0000

    added ffmpeg

--- scripts/setup/setup_system.sh
+++ scripts/setup/setup_system.sh
@@ -10,10 +10,10 @@ apt install -y \
     cuda-nvcc-12-8 \
     libgl1 \
     libglib2.0-0 \
-    wget
+    wget \
+    ffmpeg
 
 # Conda installation
 cd ~
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
-source ~/miniconda3/bin/activate
